506a940fd8fac1ebd4d6e108523af61526cc15cc
FaysalAmin77 <95224798+FaysalAmin77@users.noreply.github.com>
2021-12-21 11:12:06 -0800

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+		if (66>7){
+		    System.out.println("Number is long");
+		}
+		    else{
+		        
+		        System.out.println("Number is short");
+		    }
+		    
+		 
+	}
+}
